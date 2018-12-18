a7df86aadb144dc33aa4411f439249589c64f848
Roman Friedman <friedrom@gmail.com>
2018-12-18 17:34:12 +0700

    SkipRange stream extension.
    Allows skipping construction of unnecessary Range instances

--- json/combine_bytes/src/byterange.rs
+++ json/combine_bytes/src/byterange.rs
@@ -6,12 +6,17 @@ use std::{
         Formatter
     }
 };
+use std::borrow::Cow;
 use std::io;
+use std::marker::PhantomData;
 
 use bytes::{Buf, Bytes};
 use combine::{
     error::{
         FastResult,
+        FastResult::*,
+        Info,
+        StreamError,
         UnexpectedParse
     },
     ParseError,
@@ -26,8 +31,17 @@ use combine::{
     },
     StreamOnce
 };
+use combine::ConsumedResult;
+use combine::easy::Stream;
+use combine::error::Tracked;
+use combine::Parser;
+use combine::parser::function::parser;
+use combine::parser::ParseMode;
+use combine::stream::FullRangeStream;
+use combine::stream::input_at_eof;
 use combine::stream::state::DefaultPositioned;
 use combine::stream::state::IndexPositioner;
+use combine::stream::wrap_stream_error;
 use log::trace;
 
 #[derive(Debug, PartialEq, Eq, Clone, Hash, Default)]
@@ -55,9 +69,144 @@ pub struct BytesBuf {
     cur: io::Cursor<Bytes>
 }
 
+pub trait SkipRangeStream: RangeStream {
+    type SkipValue;
+    fn skip_while<F>(&mut self, f: F) -> Result<Self::SkipValue, StreamErrorFor<Self>> where F: FnMut(Self::Item) -> bool;
+    fn skip_range(&mut self, size: usize) -> Result<Self::SkipValue, StreamErrorFor<Self>>;
+}
+
+/// Removes items from the input while `predicate` returns `true`.
+#[inline]
+pub fn skip_stream_while<I, F>(input: &mut I, predicate: F) -> ConsumedResult<I::SkipValue, I>
+    where
+      F: FnMut(I::Item) -> bool,
+      I: ?Sized + SkipRangeStream,
+{
+    let pos = input.checkpoint();
+    match input.skip_while(predicate) {
+        Err(err) => wrap_stream_error(input, err),
+        Ok(x) => {
+            if input.is_partial() && input_at_eof(input) {
+                // Partial inputs which encounter end of file must fail to let more input be
+                // retrieved
+                ConsumedErr(I::Error::from_error(
+                    input.position(),
+                    StreamError::end_of_input(),
+                ))
+            } else if input.distance(&pos) == 0 {
+                EmptyOk(x)
+            } else {
+                ConsumedOk(x)
+            }
+        }
+    }
+}
+
+pub struct SkipWhile<I, F> {
+    f: F,
+    p: PhantomData<I>
+}
+
+impl<I, F> SkipWhile<I, F> where I: SkipRangeStream, F: FnMut(I::Item) -> bool {
+
+}
+
+pub fn skip_while<I, F>(f: F) -> SkipWhile<I, F> where I: SkipRangeStream, F: FnMut(I::Item) -> bool {
+    SkipWhile { f, p: Default::default() }
+}
+
+pub fn skip_while1<I, F>(mut f: F) -> impl Parser<Input=I, Output=I::SkipValue> where I: SkipRangeStream, F: FnMut(I::Item) -> bool {
+    parser(move |i: &mut I| {
+        let x = i.checkpoint();
+        match skip_while(&mut f).parse_stream_consumed(i) {
+            ConsumedOk(x) => ConsumedOk(x),
+            EmptyOk(_) => EmptyErr(Tracked::from(I::Error::from_error(
+                i.position(),
+                StreamError::end_of_input(),
+            ))),
+            EmptyErr(e) => EmptyErr(e),
+            ConsumedErr(e) => ConsumedErr(e)
+        }.into()
+    })
+}
+impl<I, F> Parser for SkipWhile<I, F>
+    where
+      I: SkipRangeStream,
+      F: FnMut(I::Item) -> bool,
+{
+    type Input = I;
+    type Output = I::SkipValue;
+    type PartialState = usize;
+    
+    parse_mode!();
+    #[inline]
+    fn parse_mode_impl<M>(
+        &mut self,
+        mode: M,
+        input: &mut Self::Input,
+        state: &mut Self::PartialState,
+    ) -> ConsumedResult<Self::Output, Self::Input>
+        where
+          M: ParseMode,
+    {
+        let before = input.checkpoint();
+    
+        if !input.is_partial() {
+            skip_stream_while(input, &mut self.f)
+        } else if mode.is_first() || *state == 0 {
+            let result = skip_stream_while(input, &mut self.f);
+            if let ConsumedErr(_) = result {
+                *state = input.distance(&before);
+                input.reset(before);
+            }
+            result
+        } else {
+            if input.skip_range(*state).is_err() {
+                panic!("recognize errored when restoring the input stream to its expected state");
+            }
+    
+            let r = match skip_stream_while(input, &mut self.f) {
+                ConsumedOk(r) => {
+                    *state = 0;
+                    ConsumedOk(r)
+                },
+                EmptyOk(r) => {
+                    *state = 0;
+                    EmptyOk(r)
+                },
+                EmptyErr(err) => return EmptyErr(err),
+                ConsumedErr(err) => {
+                    *state = input.distance(&before);
+                    input.reset(before);
+                    return ConsumedErr(err);
+                }
+            };
+            r
+        }
+    }
+}
+
+
+fn printbuf(mut buf: &[u8]) -> Cow<str> {
+    let b = if buf.len() > 10{
+        &buf[..10]
+    } else {
+        buf
+    };
+    String::from_utf8_lossy(b)
+}
+
 impl BytesBuf {
     pub fn new(b: Bytes) -> Self {
         BytesBuf { cur: io::Cursor::new(b) }
+    }
+    
+    #[inline]
+    pub fn slice_while<F>(&self, f: F) -> Result<&[u8], StreamErrorFor<Self>> where F: FnMut(u8) -> bool {
+        let mut slice = self.cur.bytes();
+        let result = slice.uncons_while(f);
+        //println!("Bytes slice while: {:?}", result.map(|b| String::from_utf8_lossy(b)));
+        result
     }
     
     fn buf(&self) -> &Bytes {
@@ -85,7 +234,7 @@ impl StreamOnce for BytesBuf {
     #[inline]
     fn uncons(&mut self) -> Result<u8, StreamErrorFor<Self>> {
         let next = self.cur.bytes().first();
-        //println!("Bytes uncons: {:?}", next.map(|s| (char::from(*s), String::from_utf8_lossy(self.cur.bytes()))));
+        //println!("Bytes uncons: {:?}", next.map(|s| (char::from(*s), printbuf(self.cur.bytes()))));
         let next = match next {
             Some(f) => *f,
             None => return Err(UnexpectedParse::Eoi)
@@ -95,16 +244,56 @@ impl StreamOnce for BytesBuf {
     }
 }
 
+impl SkipRangeStream for BytesBuf {
+    type SkipValue = ();
+    
+    #[inline]
+    fn skip_while<F>(&mut self, f: F) -> Result<(), StreamErrorFor<Self>> where F: FnMut(Self::Item) -> bool {
+        let len = self.slice_while(f)?.len();
+        self.cur.advance(len);
+        Ok(())
+    }
+    
+    #[inline]
+    fn skip_range(&mut self, size: usize) -> Result<(), StreamErrorFor<Self>> {
+        let result = if size < self.cur.remaining() {
+            let position = self.cur.position();
+            self.cur.set_position(position + size as u64);
+            Ok(())
+        } else {
+            Err(UnexpectedParse::Eoi)
+        };
+        //println!("Bytes skip range {:?} : {:?}", size, result);
+        result
+    }
+}
+
+impl<I> SkipRangeStream for Stream<I> where I: SkipRangeStream {
+    type SkipValue = I::SkipValue;
+    
+    #[inline]
+    fn skip_while<F>(&mut self, f: F) -> Result<I::SkipValue, StreamErrorFor<Self>> where F: FnMut(I::Item) -> bool {
+        self.0.skip_while(f).map_err(StreamError::into_other)
+    }
+    
+    #[inline]
+    fn skip_range(&mut self, size: usize) -> Result<I::SkipValue, StreamErrorFor<Self>> {
+        self.0.skip_range(size).map_err(StreamError::into_other)
+    }
+}
+
 impl Resetable for BytesBuf {
     type Checkpoint = u64;
     
+    #[inline]
     fn checkpoint(&self) -> Self::Checkpoint {
-        //println!("Checkpoint: {} {:?}", self.cur.position(), String::from_utf8_lossy(&*self.cur.bytes()));
+        //println!("Checkpoint: {} {:?}", self.cur.position(), printbuf(&*self.cur.bytes()));
         self.cur.position()
     }
     
+    #[inline]
     fn reset(&mut self, checkpoint: Self::Checkpoint) {
-        //println!("Reset: {} {} {:?}", checkpoint, self.cur.position(), String::from_utf8_lossy(&*self.cur.bytes()));
+        //println!("Reset: {} -> {} {:?}", self.cur.position(), checkpoint, printbuf(&*self.cur.bytes()));
         self.cur.set_position(checkpoint)
     }
 }
@@ -112,7 +301,6 @@ impl Resetable for BytesBuf {
 impl RangeStreamOnce for BytesBuf {
     #[inline]
     fn uncons_range(&mut self, size: usize) -> Result<BytesRange, StreamErrorFor<Self>> {
-        //println!("Uncons_range {:?} : {:?}", size, String::from_utf8_lossy(&**self.cur.get_ref()));
         let result = if size < self.cur.remaining() {
             let position = self.cur.position() as usize;
             let bytes = self.cur.get_ref().slice(position, position + size);
@@ -121,16 +309,15 @@ impl RangeStreamOnce for BytesBuf {
         } else {
             Err(UnexpectedParse::Eoi)
         };
-        //println!("Bytes uncons_range {:?} : {:?}", size, result.as_ref().map(|b| String::from_utf8_lossy(&*b.0)));
+        //println!("Bytes uncons_range {:?} : {:?}", size, result.as_ref().map(|b| printbuf(&*b.0)));
         result
     }
     
     #[inline]
-    fn uncons_while<F>(&mut self, f: F) -> Result<BytesRange, StreamErrorFor<Self>> where
+    fn uncons_while<F>(&mut self, mut f: F) -> Result<BytesRange, StreamErrorFor<Self>> where
       F: FnMut(Self::Item) -> bool {
-        let mut slice = self.cur.bytes();
-        let result = slice.uncons_while(f);
-        //println!("Bytes uncons_while: {:?}", result.map(|b| String::from_utf8_lossy(b)));
+        //println!("Uncons while");
+        let result = self.slice_while(&mut f);
         match result {
             Ok(s) if s.len() > 0 => {
                 let bytes = self.buf().slice_ref (s);
@@ -150,7 +337,7 @@ impl RangeStreamOnce for BytesBuf {
         use self::FastResult::*;
         let mut slice = &*self.cur.bytes();
         let result = slice.uncons_while1(f);
-        //println!("Bytes uncons_while1: {:?}", result.map(|b| String::from_utf8_lossy(b)));
+        //println!("Bytes uncons_while1: {:?}", result.map(|b| printbuf(b)));
         match result {
             ConsumedOk(s) => {
                 let bytes = self.cur.get_ref().slice_ref(s);
@@ -165,7 +352,7 @@ impl RangeStreamOnce for BytesBuf {
     
     fn distance(&self, end: &u64) -> usize {
         let dist = self.cur.position() as usize - *end as usize;
-        //println!("Bytes distance1 {:?} : {:?} res: {:?} {:?}", self.cur.position(), end, dist, String::from_utf8_lossy(&*self.cur.bytes()));
+        //println!("Bytes distance {:?} -> {:?} res: {:?} {:?}", self.cur.position(), end, dist, printbuf(&*self.cur.bytes()));
         dist
     }
 }
--- json/combine_bytes/src/main.rs
+++ json/combine_bytes/src/main.rs
@@ -10,16 +10,19 @@ use bencher::{Bencher, black_box};
 use bytes::Bytes;
 use combine::{
     any,
-    error::Consumed,
-    error::ParseError,
-    error::ParseResult2,
+    error::{
+        Consumed,
+        Info,
+        ParseError,
+        ParseResult2
+    },
     Parser,
     parser,
     parser::{
         byte::{byte, bytes, digit},
         choice::{choice, optional},
         combinator::{ignore, no_partial},
-        item::{one_of, satisfy_map},
+        item::{self, one_of, satisfy_map, tokens},
         range,
         repeat::{escaped, many, many1, sep_by},
         sequence::between,
@@ -37,9 +40,14 @@ use combine::{
 use combine::range::TakeWhile1;
 
 use crate::{
+    byterange::BytesBuf,
     byterange::BytesRange,
-    byterange::BytesBuf
+    byterange::skip_while,
+    byterange::SkipRangeStream,
+    byterange::SkipWhile
 };
+use std::result::Result::Ok;
+use crate::byterange::skip_while1;
 
 pub mod byterange;
 
@@ -53,16 +61,16 @@ enum Value {
     Array(Vec<Value>),
 }
 
-fn spaces<I>() -> impl Parser<Input=I, Output=BytesRange>
+fn spaces<I>() -> impl Parser<Input=I, Output=I::SkipValue>
     where
-      I: RangeStream<Item=u8, Range=BytesRange>,
+      I: SkipRangeStream<Item=u8, Range=BytesRange>,
       I::Error: ParseError<
           I::Item,
           I::Range,
           I::Position,
       >
 {
-    range::take_while(|b| {
+    skip_while(|b| {
         b == b' ' || b == b'\t' || b == b'\r' || b == b'\n'
     })
 }
@@ -70,7 +78,7 @@ fn spaces<I>() -> impl Parser<Input=I, Output=BytesRange>
 fn lex<P>(p: P) -> impl Parser<Input=P::Input, Output=P::Output>
     where
       P: Parser,
-      P::Input: RangeStream<Item=u8, Range=BytesRange>,
+      P::Input: SkipRangeStream<Item=u8, Range=BytesRange>,
       <P::Input as StreamOnce>::Error: ParseError<
           <P::Input as StreamOnce>::Item,
           <P::Input as StreamOnce>::Range,
@@ -83,34 +91,34 @@ fn lex<P>(p: P) -> impl Parser<Input=P::Input, Output=P::Output>
 fn lex_around<P>(p: P) -> impl Parser<Input=P::Input, Output=P::Output>
     where
       P: Parser,
-      P::Input: RangeStream<Item=u8, Range=BytesRange>,
+      P::Input: SkipRangeStream<Item=u8, Range=BytesRange>,
       <P::Input as StreamOnce>::Error: ParseError<
           <P::Input as StreamOnce>::Item,
           <P::Input as StreamOnce>::Range,
           <P::Input as StreamOnce>::Position,
       >
 {
-    (no_partial(ignore(spaces())), p, no_partial(ignore(spaces())))
+    (no_partial(spaces()), p, no_partial(spaces()))
                  //this doesn't look very good :)
                  .map(|(_, o, _)| o)
 }
 
 
-fn digits<I>() -> TakeWhile1<I, fn(I::Item) -> bool>
+fn digits<I>() -> impl Parser<Input=I, Output=I::SkipValue>
     where
-      I: RangeStream<Item=u8, Range=BytesRange>,
+      I: SkipRangeStream<Item=u8, Range=BytesRange>,
       I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
-    range::take_while1(|b| b >= b'0' && b <= b'9')
+    skip_while1(|b| b >= b'0' && b <= b'9')
 }
 
 fn number<I>() -> impl Parser<Input=I, Output=BytesRange>
     where
-      I: RangeStream<Item=u8, Range=BytesRange>,
+      I: SkipRangeStream<Item=u8, Range=BytesRange>,
       I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     no_partial(
-        range::recognize(no_partial((
+        inspect("number", range::recognize(no_partial((
             optional(one_of("+-".bytes())),
             choice((
                 (digits(), optional((byte(b'.'), optional(digits())))).map(|_| ()),
@@ -120,13 +128,13 @@ fn number<I>() -> impl Parser<Input=I, Output=BytesRange>
                 (one_of("eE".bytes()), optional(one_of("+-".bytes()))),
                 digits(),
             )),
-        ))).expected("number"),
+        ))))
     )
 }
 
 fn json_string<I>() -> impl Parser<Input=I, Output=BytesRange>
     where
-      I: RangeStream<Item=u8, Range=BytesRange>,
+      I: SkipRangeStream<Item=u8, Range=BytesRange>,
       I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let back_slash_byte = satisfy_map(|c| {
@@ -143,59 +151,82 @@ fn json_string<I>() -> impl Parser<Input=I, Output=BytesRange>
         })
     });
     let inner = range::recognize(escaped(
-        range::take_while1(|b| b != b'\\' && b != b'"'),
+        skip_while1(|b| b != b'\\' && b != b'"'),
         b'\\',
         back_slash_byte,
     ));
-    between(byte(b'"'), byte(b'"'), inner).expected("string")
+    inspect("string", between(byte(b'"'), byte(b'"'), inner))
 }
 
 fn object<I>() -> impl Parser<Input=I, Output=Vec<(Bytes, Value)>>
     where
-      I: RangeStream<Item=u8, Range=BytesRange>,
+      I: SkipRangeStream<Item=u8, Range=BytesRange>,
       I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     let field = (json_string().map(|bytes| bytes.0), lex(byte(b':')), json_value()).map(|t| (t.0, t.2));
     let fields = sep_by(field, lex_around(byte(b',')));
-    between(byte(b'{').skip(spaces()), lex(byte(b'}')), fields).expected("object")
+    inspect("object",
+    between(byte(b'{').skip(spaces()), lex(byte(b'}')), fields))
 }
 
 fn array<I>() -> impl Parser<Input=I, Output=Vec<Value>>
     where
-      I: RangeStream<Item=u8, Range=BytesRange>,
+      I: SkipRangeStream<Item=u8, Range=BytesRange>,
       I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
-    between(
+    inspect("array", between(
         byte(b'['),
         lex(byte(b']')),
         sep_by(json_value(), lex(byte(b','))),
-    ).expected("array")
+    ))
 }
 
 #[inline(always)]
 fn json_value<I>() -> impl Parser<Input=I, Output=Value>
     where
-      I: RangeStream<Item=u8, Range=BytesRange>,
+      I: SkipRangeStream<Item=u8, Range=BytesRange>,
       I::Error: ParseError<I::Item, I::Range, I::Position>,
 {
     lex_around(json_value_())
 }
 
+
+fn value<I>(buf: &'static str) -> impl Parser<Input=I, Output=()>
+    where I: SkipRangeStream<Item=u8, Range=BytesRange>,
+          I::Error: ParseError<I::Item, I::Range, I::Position> {
+    inspect(buf, tokens(|l,r| l == &r, Info::Range(BytesRange(Bytes::from_static(buf.as_bytes()))), buf.as_bytes().iter())
+      .map(|_| ()))
+}
+
+fn inspect<P>(buf: &'static str, p: P) -> impl Parser<Input=P::Input, Output=P::Output>
+    where P: Parser,
+          P::Input: SkipRangeStream<Item=u8, Range=BytesRange>,
+          <P::Input as StreamOnce>::Error: ParseError<
+              <P::Input as StreamOnce>::Item,
+              <P::Input as StreamOnce>::Range,
+              <P::Input as StreamOnce>::Position> {
+    
+    parser(move |input| {
+        //println!("Parser {}", buf);
+        Ok(((), Consumed::Empty(())))
+    }).with(p)
+      .expected(buf)
+}
 // We need to use `parser!` to break the recursive use of `value` to prevent the returned parser
 // from containing itself
 parser! {
     #[inline(always)]
     fn json_value_[I]()(I) -> Value
-        where [ I: RangeStream<Item = u8, Range = BytesRange> ]
+        where [ I: SkipRangeStream<Item = u8, Range = BytesRange> ]
     {
         choice((
             json_string().map(|b| Value::String(b.0)),
             object().map(Value::Object),
             array().map(Value::Array),
             number().map(|b| Value::Number(b.0)),
-            range::range(BytesRange(Bytes::from_static(b"false"))).map(|_| Value::Bool(false)),
-            range::range(BytesRange(Bytes::from_static(b"true"))).map(|_| Value::Bool(true)),
-            range::range(BytesRange(Bytes::from_static(b"null"))).map(|_| Value::Null),
+            value("false").map(|_| Value::Bool(false)),
+            value("true").map(|_| Value::Bool(true)),
+            value("null").map(|_| Value::Null),
         ))
     }
 }
@@ -206,7 +237,7 @@ fn json_test() {
     use bytes::Bytes;
     use self::BytesRange;
     let input = br#"{
-        "array" : [1, ""],
+        "array": [1, ""],
         "object" :  {},
         "number" : 3.14,
         "small_number": 0.59,
@@ -241,7 +272,7 @@ fn json_test() {
     match result {
         Ok((result, input)) => assert_eq!(result, expected),
         Err(e) => {
-            println!("{}\n{:?}", e, e);
+            //println!("{}\n{:?}", e, e);
             assert!(false);
         }
     }
@@ -258,7 +289,7 @@ fn test() {
     
     let mut parser = json_value();
     let result = parser.easy_parse(BytesBuf::new(Bytes::from_static(data.as_bytes())));
-    println!("test: {:?}", result);
+    //println!("test: {:?}", result);
     result.unwrap();
 }
 
@@ -267,7 +298,7 @@ fn apache_test() {
     let data = include_str!("../../apache_builds.json");
     let mut parser = json_value();
     let result = parser.easy_parse(BytesBuf::new (Bytes::from_static(data.as_bytes())));
-    println!("test: {:?}", result);
+    //println!("test: {:?}", result);
     result.unwrap();
 }
 
@@ -314,7 +345,7 @@ fn apache(b: &mut Bencher) {
 
 //deactivating the "basic" benchmark because the parser fails on this one
 //benchmark_group!(json, basic, data, apache, canada);
-benchmark_group!(json, basic, data, apache, canada);
+benchmark_group!(json, data);
 benchmark_main!(json);
 
 /*
